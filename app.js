33d07986ac4526e0997505f2105ca706f62c6354
Milan Koniar <milankoniar@gmail.com>
2019-04-22 11:25:11 +0200

    setup a basic endpoint to get posts

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First Middleware');
-    next();
-});
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        { id: 'kjhkjh', title: 'Title 1', content: 'content 1' },
+        { id: 'sggdrgd', title: 'Title 2', content: 'content 2' },
+        { id: 'kujhkjhk', title: 'Title 3', content: 'content 3' }
+    ];
 
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+    res.status(200).json({
+        msg: 'Posts fetched succesfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
