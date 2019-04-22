34381477192b0ac162ea6f2c94365c9f52d06107
Milan Koniar <milankoniar@gmail.com>
2019-04-22 17:13:59 +0200

    dealt with the CORS

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
+    next();
+});
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
-        { id: 'kjhkjh', title: 'Title 1', content: 'content 1' },
+        { id: 'kjhkjh', title: 'Title 1', content: 'content 1..' },
         { id: 'sggdrgd', title: 'Title 2', content: 'content 2' },
         { id: 'kujhkjhk', title: 'Title 3', content: 'content 3' }
     ];
