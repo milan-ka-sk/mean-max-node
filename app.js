3aed9df29a0d733f1a5051c3bf64a89f739014f8
Milan Koniar <milankoniar@gmail.com>
2019-04-22 17:33:51 +0200

    setup the post endpoint  partially

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
     next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).joson({
+        msg: 'Post added succesfully.'
+    });
 });
 
 app.use('/api/posts', (req, res, next) => {
